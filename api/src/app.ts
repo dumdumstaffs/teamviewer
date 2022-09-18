52322814bb2a8a4f3335259f338d9cb8441698d0
DumDum Staffs <dumdumstaffs@gmail.com>
2022-09-18 06:51:09 +0100

    add helmet

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import * as dotenv from "dotenv";
 import express, { NextFunction, Request, Response } from "express";
+import helmet from "helmet";
 import * as db from "./db";
 import { ServerError } from "./error";
 import * as modules from "./modules";
@@ -11,6 +12,7 @@ db.connectDb()
 export const app = express()
 
 app.use(cors())
+app.use(helmet())
 app.use(express.json({ limit: "10mb" }))
 app.use(express.urlencoded({ limit: "10mb", extended: true }))
 
